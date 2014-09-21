f4f61b6e75c8e157e09f80cd50b16604d4986bd2
Kermitthefrog <johanssonjohan@outlook.com>
2014-09-21 22:55:17 +0200

    To pi

--- assets/js/concat.js
+++ assets/js/concat.js
@@ -1,3 +1,57 @@
-/*! Pliny - v0.1.0 - 2013-11-25
-*  
-* Copyright (c) 2013 Adam D. Scott; Licensed under the MIT License */
+/*! johanjohnasson - v0.1.0 - 2014-09-21
+* www.johanjohansson.me 
+* Copyright (c) 2014 Johan Johansson; Licensed under the MIT License */
+$(document).ready(function() {
+	// Load Background Images
+	var images = ['1.jpg', '2.png', 'eng.jpg', 'tog.jpg', 'apple.jpg', 'hav.jpg'];
+ 	$("#header").css({
+ 			'background': 'url(assets/img/'
+ 				+ images[Math.floor(Math.random() * images.length)] + ')'
+ 	});
+
+    // Load Background Images
+  var images = ['1.jpg', 'tog.jpg', 'apple.jpg', 'hav.jpg'];
+  $("#post-header").css({
+      'background': 'url(../assets/img/second-header/'
+        + images[Math.floor(Math.random() * images.length)] + ')'
+  });
+
+
+ 	$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+// If scroll is bigger then 200 do menu fixed & add class avtive
+  $(function() {
+    var header = $(".fixed-menu");
+    $(".fixed-menu").addClass("is-hidden");
+
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll <= 700) {
+        header.addClass("is-hidden");
+      } else {
+          header.removeClass("is-hidden");
+      }
+    });
+  });
+
+
+
+
+});
---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	// Load Background Images
+	var images = ['1.jpg', '2.png', 'eng.jpg', 'tog.jpg', 'apple.jpg', 'hav.jpg'];
+ 	$("#header").css({
+ 			'background': 'url(assets/img/'
+ 				+ images[Math.floor(Math.random() * images.length)] + ')'
+ 	});
+
+    // Load Background Images
+  var images = ['1.jpg', 'tog.jpg', 'apple.jpg', 'hav.jpg'];
+  $("#post-header").css({
+      'background': 'url(../assets/img/second-header/'
+        + images[Math.floor(Math.random() * images.length)] + ')'
+  });
+
+
+ 	$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+// If scroll is bigger then 200 do menu fixed & add class avtive
+  $(function() {
+    var header = $(".fixed-menu");
+    $(".fixed-menu").addClass("is-hidden");
+
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll <= 700) {
+        header.addClass("is-hidden");
+      } else {
+          header.removeClass("is-hidden");
+      }
+    });
+  });
+
+
+
+
+});
