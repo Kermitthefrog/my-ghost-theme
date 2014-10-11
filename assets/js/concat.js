bfcb7deacd2b0d02d92c7d5d50b1ce19dea6be8c
Kermitthefrog <johanssonjohan@outlook.com>
2014-10-11 21:07:32 +0200

    Update Front & Menu

--- assets/js/concat.js
+++ assets/js/concat.js
@@ -1,13 +1,13 @@
-/*! johanjohnasson - v0.1.0 - 2014-10-05
+/*! johanjohnasson - v0.1.0 - 2014-10-11
 * www.johanjohansson.me 
 * Copyright (c) 2014 Johan Johansson; Licensed under the MIT License */
 $(document).ready(function() {
 	// Load Background Images
-	var images = ['1.jpg', '2.png', 'eng.jpg', 'tog.jpg', 'apple.jpg', 'hav.jpg'];
- 	$("#header").css({
- 			'background': 'url(assets/img/'
- 				+ images[Math.floor(Math.random() * images.length)] + ')'
- 	});
+	//var images = ['1.jpg', '2.png', 'eng.jpg', 'tog.jpg', 'apple.jpg', 'hav.jpg'];
+ 	//$("#header").css({
+ 		//	'background': 'url(assets/img/'
+ 			//	+ images[Math.floor(Math.random() * images.length)] + ')'
+ 	//});
 
   // Qoutes on FrontPage
   function showQuotes() {
@@ -50,28 +50,6 @@ showQuotes();
   });
 });
 
-// If scroll is bigger then 200 do menu fixed & add class avtive
-  $(function() {
-    var header = $(".fixed-menu");
-    var largenav = $(".primary-navigation");
-    var nav = $(".header-bar");
-    $(".fixed-menu").addClass("is-hidden");
-
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-
-      if (scroll <= 730) {
-       // header.addClass("is-hidden");
-        //nav.removeClass("fixed");
-        //largenav.removeClass("fixed");
-      } else {
-         // header.removeClass("is-hidden");
-          //nav.addClass("fixed");
-          //largenav.addClass("fixed");
-      }
-    });
-  });
-
 // mobile
   var $links = $('.overlay');
   var citat = $('.qoute-mobile');
@@ -87,13 +65,11 @@ showQuotes();
 
   preload();
 
-  $('.rollover').on('mouseenter',function(){
+  $('.rollover').on('mouseenter',function() {
     switchSrc($(this));
   })
 
-  $('.rollover').on('mouseleave',function(){
+  $('.rollover').on('mouseleave',function() {
     switchSrc($(this));
   })
-
-
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	// Load Background Images
-	var images = ['1.jpg', '2.png', 'eng.jpg', 'tog.jpg', 'apple.jpg', 'hav.jpg'];
- 	$("#header").css({
- 			'background': 'url(assets/img/'
- 				+ images[Math.floor(Math.random() * images.length)] + ')'
- 	});
+	//var images = ['1.jpg', '2.png', 'eng.jpg', 'tog.jpg', 'apple.jpg', 'hav.jpg'];
+ 	//$("#header").css({
+ 		//	'background': 'url(assets/img/'
+ 			//	+ images[Math.floor(Math.random() * images.length)] + ')'
+ 	//});
 
   // Qoutes on FrontPage
   function showQuotes() {
@@ -47,28 +47,6 @@ showQuotes();
   });
 });
 
-// If scroll is bigger then 200 do menu fixed & add class avtive
-  $(function() {
-    var header = $(".fixed-menu");
-    var largenav = $(".primary-navigation");
-    var nav = $(".header-bar");
-    $(".fixed-menu").addClass("is-hidden");
-
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-
-      if (scroll <= 730) {
-       // header.addClass("is-hidden");
-        //nav.removeClass("fixed");
-        //largenav.removeClass("fixed");
-      } else {
-         // header.removeClass("is-hidden");
-          //nav.addClass("fixed");
-          //largenav.addClass("fixed");
-      }
-    });
-  });
-
 // mobile
   var $links = $('.overlay');
   var citat = $('.qoute-mobile');
@@ -84,13 +62,11 @@ showQuotes();
 
   preload();
 
-  $('.rollover').on('mouseenter',function(){
+  $('.rollover').on('mouseenter',function() {
     switchSrc($(this));
   })
 
-  $('.rollover').on('mouseleave',function(){
+  $('.rollover').on('mouseleave',function() {
     switchSrc($(this));
   })
-
-
 });
